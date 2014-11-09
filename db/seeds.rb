022ddcd02857a88e0fde3b0b138775a58bae5d0c
Andrew Archer <andrew.m.archer@gmail.com>
2014-11-08 19:55:30 -0800

    db seed file

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+require "csv"
+
+FILE = APP_ROOT.join('db','TestReceipt.txt')
+
+[Tab,Rabbit,Item].each do |table|
+	table.send(:delete_all)
+end
+
+
+file = CSV.read(FILE, {:col_sep => "\t"})
+tab = Tab.create()
+
+file.each do |item|
+	name = item[0]
+	price = 100 * item[1].to_i
+	Item.create(name: name, price: price, tab_id: tab.id)
+end
+
+Rabbit.create(	name: 'Andrew',
+								email: 'andrew.m.archer@gmail.com',
+								phone_number: '415-555-5555'
+							)
