f3f78dcd00eec548d9e3ba2bc5b2f3b2512d1fc6
Andrew Archer <andrew.m.archer@gmail.com>
2014-11-12 15:23:32 -0800

    better tab view

--- app/controllers/rabbit.rb
+++ app/controllers/rabbit.rb
@@ -12,20 +12,30 @@ post '/tab/:tab_id/rabbit/new' do
 	end
 end
 
+# edit the rabbit
+put '/rabbit/:rabbit_id' do
+	rabbit = Rabbit.find(params[:rabbit_id])
+	if rabbit.update_params(params)
+		content_type :json
+		rabbit.to_json
+	else
+		halt 400, rabbit.errors.to_json
+	end
+end
+
 # remove the rabbit from the tab (but not from the DB!)
 delete '/tab/:tab_id/rabbit/:rabbit_id' do
 	tab = Tab.find(params[:tab_id])
 	rabbit = Rabbit.find(params[:rabbit_id])
 	if tab && tab.rabbits.delete(rabbit)
 		content_type :json
-		p rabbit.to_json
 		{id: rabbit.id}.to_json
 	else
 		halt 400, rabbit.errors.to_json
 	end
 end
 
-# remove the rabbit
+# remove the rabbit entirely
 delete '/rabbit/:rabbit_id' do
 	rabbit = Tab.find(params[:rabbit_id])
 	if rabbit.destroy
